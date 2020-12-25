a13eea98e992b488beb50697f7db281038ffdd7c
sony <qqfelix587@gmail.com>
2020-12-25 18:07:18 +0900

    #4. ROOM APP and the other App, Models and Queryset

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,82 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    """ Item Admin Definition """
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """ Room Admin Definition """
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book"), }
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")}
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                # 숨기기 기능
+                "fields": ("amenities", "facilities", "house_rules")}
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)}
+        )
+    )
+    ordering = ('name', "price",)
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities"
+
+    )
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+    search_fields = ("=city", "^host__username")
+    # https://docs.djangoproject.com/en/3.1/ref/contrib/admin/이곳을 참고
+
+    def count_amenities(self, obj):
+        # self: admin class , obj : current row
+        return obj.amenities.count()
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ """
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -1,10 +1,55 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 # 순서는 django -> third party -> myapp
 
-class
+
+class AbstractItem(core_models.TimeStampedModel):
+    """ Abstract Item """
+    name = models.CharField(max_length=80)
+    # name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract: True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    """ RoomType Object Definition """
+    class Meta:
+        verbose_name = "Room Type"
+        # ordering = ['name']
+
+
+class Amenity(AbstractItem):
+    """ Amenity Object Definition """
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    """ Facility Model Definition """
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    """ Houserule Model Definition """
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    # Room으로 쓰면 파이썬의 읽는 방식에 의해 에러발생. foreignkey는 "" 형식을 사용가능
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -23,8 +68,15 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE)
     # room과 user를 connect / many to one relation : many rooms -> user
+    # on_delete의 옵션은 다음의 사이트에서 확인 https://brunch.co.kr/@ddangdol/5
+    room_type = models.ForeignKey(
+        "RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
